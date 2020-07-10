0cd4b79e34f4890a7e8209a9d54d28920fbd7712
Shannon Lounges <shannonlounges@Shannons-MacBook-Pro.local>
2020-07-10 17:42:04 -0400

    updating index

--- js/main.js
+++ js/main.js
@@ -1,19 +1,15 @@
 let Stickers = document.querySelectorAll('.svgSticker')
-
-function whenClicked(EventObject) {
+function clickSticker(EventObject) {
     let clickedIcon = EventObject.currentTarget;
     console.log(`the sticker you are adding to the cart is the: ${clickedIcon.id}`)
     clickedIcon.classList.toggle('selected')
-    updateCount()
+    countStickers()
 }
-
 Stickers.forEach(function(icon) {
-    icon.addEventListener('click', whenClicked, true)
+    icon.addEventListener('click', clickSticker, true)
 })
-
-
-function updateCount() {
-    const countElement = document.querySelector('.count')
+function countStickers() {
+    const countElement = document.querySelector('.basket')
     countElement.dataset.count = document.querySelectorAll('.svgSticker.selected').length
     countElement.innerHTML = countElement.dataset.count
 }
