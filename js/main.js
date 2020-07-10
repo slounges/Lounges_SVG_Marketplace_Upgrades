ac340817a9f12f9e5e656ee5e67791eb61efa23a
Shannon Lounges <shannonlounges@Shannons-MacBook-Pro.local>
2020-07-10 14:12:04 -0400

    adding upgrades

--- js/main.js
+++ js/main.js
@@ -1,34 +1,25 @@
-(() => {
-	console.log('selected!');
-	
 
-let OneStick = document.querySelector(".product-image"),
-TwoStick = document.querySelector(".product-image2"),
-ThreeStick =document.querySelector(".product-image3"),
-FourStick =document.querySelector(".product-image4"),
-FiveStick =document.querySelector(".product-image5"),
-SixStick =document.querySelector(".product-image6"),
-SevenStick =document.querySelector(".product-image7"),
-EightStick =document.querySelector(".product-image8"),
-NineStick =document.querySelector(".product-image9"),
-TenStick =document.querySelector(".product-image10");
 
-function logID(){
-	console.log("you are adding this sticker to the cart:",this.id);
+let iconSet = document.querySelectorAll('.svgIcon')
+
+function whenClicked(EventObject) {
+    let clickedIcon = EventObject.currentTarget;
+
+    console.log(`the sticker you are adding to the cart is the: ${clickedIcon.id}`)
+
+
+    clickedIcon.classList.toggle('selected')
+    updateCount()
 }
 
-OneStick.addEventListener("click", logID);
-TwoStick.addEventListener("click", logID);
-ThreeStick.addEventListener("click", logID);
-FourStick.addEventListener("click", logID);
-FiveStick.addEventListener("click", logID);
-SixStick.addEventListener("click", logID);
-SevenStick.addEventListener("click", logID);
-EightStick.addEventListener("click", logID);
-NineStick.addEventListener("click", logID);
-TenStick.addEventListener("click", logID);
-})();
+iconSet.forEach(function(icon) {
+    icon.addEventListener('click', whenClicked, true)
+})
 
 
+function updateCount() {
 
-
+    const countElement = document.querySelector('.count')
+    countElement.dataset.count = document.querySelectorAll('.svgIcon.selected').length
+    countElement.innerHTML = countElement.dataset.count
+}
